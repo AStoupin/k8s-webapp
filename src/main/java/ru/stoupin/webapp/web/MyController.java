ccd9e12b8cd04d570831eab4633f3d89822ebe9f
Art <art@stoupin.ru>
2020-03-09 18:14:45 +0300

    ok

--- src/main/java/ru/stoupin/webapp/web/MyController.java
+++ src/main/java/ru/stoupin/webapp/web/MyController.java
@@ -37,7 +37,7 @@ public class MyController {
 	    // try to load from maven properties first
 	    try {
 	        Properties p = new Properties();
-	        InputStream is = getClass().getResourceAsStream("/META-INF/maven/ru.stoupin.webapp/webapp/pom.properties");
+	        InputStream is = getClass().getResourceAsStream("/META-INF/maven/ru.stoupin/webapp/pom.properties");
 	        if (is != null) {
 	            p.load(is);
 	            version = p.getProperty("version", "");
